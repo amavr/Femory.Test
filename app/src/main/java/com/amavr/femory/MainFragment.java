02a6db606f4d87928c71f08518241d9f6735ee82
amavr <albert.mavrin@gmail.com>
2020-04-21 21:00:51 +0300

    Навигация и отображения главного фрагмента

--- app/src/main/java/com/amavr/femory/MainFragment.java
+++ app/src/main/java/com/amavr/femory/MainFragment.java
@@ -3,6 +3,8 @@ package com.amavr.femory;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,10 +13,10 @@ import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link TasksFragment#newInstance} factory method to
+ * Use the {@link MainFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class TasksFragment extends Fragment {
+public class MainFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -24,7 +26,9 @@ public class TasksFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public TasksFragment() {
+    private NavController nc;
+
+    public MainFragment() {
         // Required empty public constructor
     }
 
@@ -34,11 +38,11 @@ public class TasksFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment TasksFragment.
+     * @return A new instance of fragment NavFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static TasksFragment newInstance(String param1, String param2) {
-        TasksFragment fragment = new TasksFragment();
+    public static MainFragment newInstance(String param1, String param2) {
+        MainFragment fragment = new MainFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -59,6 +63,30 @@ public class TasksFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tasks, container, false);
+        View navView = inflater.inflate(R.layout.fragment_main, container, false);
+        nc = NavHostFragment.findNavController(this);
+
+//        navView.findViewById(R.id.btnTasks).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                nc.navigate(R.id.tasksFragment);
+//            }
+//        });
+//
+//        navView.findViewById(R.id.btnUsers).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                nc.navigate(R.id.usersFragment);
+//            }
+//        });
+//
+//        navView.findViewById(R.id.btnOptions).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                nc.navigate(R.id.optionsFragment);
+//            }
+//        });
+
+        return navView;
     }
 }
